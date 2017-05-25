3495a6cb15d7e8e36efb75be6bfb0ac299df90ea
Anton Ostrovskiy <ao.omega2000@gmail.com>
2017-05-25 00:07:55 -0500

    Corrected vertices

--- AO/Learning/src/Prime-Transform.cpp
+++ AO/Learning/src/Prime-Transform.cpp
@@ -29,7 +29,7 @@ void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods
 
 int main(int argc, const char* argv[]){
 
-    system("COLOR F0");
+    system("COLOR 0A");
     int width, height;
 
     glfwInit();
@@ -60,14 +60,15 @@ int main(int argc, const char* argv[]){
     std::string parentDir(getParentDirectory(argv[0]));
     GLuint shaderProg1 = compileShaders(parentDir, "AO-1.vert", "AO-1.frag");
     GLuint shaderProg2 = compileShaders(parentDir, "AO-2.vert", "AO-2.frag");
-    glUseProgram(shaderProg1);
+    GLuint shaderProg3 = compileShaders(parentDir, "AO-3.vert", "AO-3.frag");
+    glUseProgram(shaderProg3);
     // result = glGetError();
     // GLuint shaderProg = initLoadShaders();
 
     // const char* absoluteKTX1 = "D:\\AntonDocs\\Codex\\Mono-Canvas\\MinGW-64-V3\\AO\\AO-Projects\\Learning\\Textures\\KTX\\Container.KTX";
     // const char* absoluteKTX2 = "D:\\AntonDocs\\Codex\\Mono-Canvas\\Git-Mingw-64\\AO\\AO\\Learning\\data\\ktx\\Metal-1.ktx";
     // GLuint myTexture = produceTexture2(absoluteKTX);
-    std::string fileKTX = "\\data\\ktx\\Texture8_PNG_BC7_1.KTX";
+    /* std::string fileKTX = "\\data\\ktx\\Texture8_PNG_BC7_1.KTX";
     std::string pathKTX = parentDir + fileKTX;
     const char* c_pathKTX = pathKTX.c_str();
     std::cout << "Relative path to KTX file is " << pathKTX << std::endl;
@@ -78,7 +79,7 @@ int main(int argc, const char* argv[]){
         return 1;
     } else {
         std::cout << "Texture created successfuly from file: \n" << c_pathKTX << std::endl;
-    }
+    } */
 
     struct Vertex {
         GLfloat pos[3];
@@ -118,50 +119,20 @@ int main(int argc, const char* argv[]){
     };
 
     Vertex verts3[] = {
-        { {0.7f, 0.7f, 0}, {63, 127, 191, 255} }, // INDEX 0
-        { {0.7f, -0.7f, 0}, {219, 97, 99, 255} }, // INDEX 1
-        { {-0.7f, 0.7f, 0}, {126, 251, 138, 255} }, // INDEX 2
-        { {-0.7f, -0.7f, 0}, {181, 127, 98, 255} }, // INDEX 3
+        { {0.7f, 0.5f, 0}, {63, 127, 191, 255} }, // INDEX 0
+        { {0.7f, -0.5f, 0}, {219, 97, 99, 255} }, // INDEX 1
+        { {-0.7f, 0.9f, 0}, {126, 251, 138, 255} }, // INDEX 2
+        { {-0.7f, -0.9f, 0}, {178, 137, 204, 255} }, // INDEX 3
 
-        { {0.5f, 0.5f, 0}, {63, 127, 191, 255} }, // INDEX 4
-        { {0.5f, -0.5f, 0}, {219, 97, 99, 255} }, // INDEX 5
-        { {-0.5f, 0.5f, 0}, {126, 251, 138, 255} }, // INDEX 6
-        { {-0.5f, -0.5f, 0}, {181, 127, 98, 255} }, // INDEX 7
+        { {0.5f, 0.7f, 0}, {63, 127, 191, 255} }, // INDEX 0
+        { {0.5f, -0.7f, 0}, {219, 97, 99, 255} }, // INDEX 1
+        { {-0.5f, 0.9f, 0}, {126, 251, 138, 255} }, // INDEX 2
+        { {-0.5f, -0.9f, 0}, {178, 137, 204, 255} }, // INDEX 3
 
-        { {0.3f, 0.3f, 0}, {63, 127, 191, 255} }, // INDEX 8
-        { {0.3f, -0.3f, 0}, {219, 97, 99, 255} }, // INDEX 9
-        { {-0.3f, 0.3f, 0}, {126, 251, 138, 255} }, // INDEX 10
-        { {-0.3f, -0.3f, 0}, {181, 127, 98, 255} }, // INDEX 11
-    };
-
-    /* GLfloat texCoord[] = {
-        0.75f, 0.75f,
-        0.75f, 0.25f,
-        0.25f, 0.75f
-    }; */
-
-    /* GLfloat texCoord[] = {
-        0.75f / 3, 0.75f / 3,
-        0.75f / 3, 0.25f / 3,
-        0.25f / 3, 0.75f / 3
-    }; */
-
-    /* GLfloat texCoord[] = {
-        0.75f * 1.5, 0.75f * 1.5,
-        0.75f * 1.5, 0.25f * 1.5,
-        0.25f * 1.5, 0.75f * 1.5
-    }; */
-
-    GLfloat texCoord[] = {
-        0.75f * 2, 0.75f * 2,
-        0.75f * 2, 0.25f * 2,
-        0.25f * 2, 0.75f * 2
-    };
-
-    GLfloat texCoord2[] = {
-        1.0f * 3, 1.0f * 3,
-        1.0f * 3, 0.0f * 3,
-        0.0f * 3, 1.0f * 3
+        { {0.9f, 0.5f, 0}, {63, 127, 191, 255} }, // INDEX 0
+        { {0.9f, -0.5f, 0}, {219, 97, 99, 255} }, // INDEX 1
+        { {-0.9f, 0.5f, 0}, {126, 251, 138, 255} }, // INDEX 2
+        { {-0.9f, -0.5f, 0}, {178, 137, 204, 255} }, // INDEX 3
     };
 
     GLuint indices[] = {
@@ -174,7 +145,22 @@ int main(int argc, const char* argv[]){
         2, 1, 3
     };
 
-    GLuint indices3[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+    GLuint indices3[] = {
+        0, 5 + 3, 10,
+        // 1, 6, 11,
+        5, 7, 8,
+        // 3, 4, 9
+    };
+
+    GLuint indices4[] = {
+        // 5, 7, 8,
+        0, 4, 8,
+        0 + 1, 4 + 1, 8 + 1,
+        0 + 2, 4 + 2, 8 + 2,
+        0 + 3, 4 + 3, 8 + 3
+    };
+
+    std::cout << "result: " << glGetError() << std::endl;
 
     GLuint VBO, VAO, EBO;
     GLuint VBOs[2];
@@ -184,26 +170,23 @@ int main(int argc, const char* argv[]){
     glGenBuffers(2, VBOs);
     glBindVertexArray(VAO);
 
+
     glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(verts2), verts2, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(verts3), verts3, GL_STATIC_DRAW);
     // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat)  , (GLvoid*)0);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, pos));
     glEnableVertexAttribArray(0);
     // Task: Make sure variable at layout (location = ...) exists, otherwise yield error message
     glVertexAttribPointer(1, 4, GL_UNSIGNED_BYTE, GL_TRUE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, color));
-    glEnableVertexAttribArray(1);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(texCoord2), texCoord2, GL_STATIC_DRAW);
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 0, nullptr);
-    glEnableVertexAttribArray(2);
+    // glVertexAttribIPointer(1, 4, GL_UNSIGNED_BYTE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, color));
+    glEnableVertexAttribArray(1); // h
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices2), indices2, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices4), indices4, GL_STATIC_DRAW);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0); // Unbinding
     glBindVertexArray(0); // Unbinding
-
+    int cnt = 0;
     while (!glfwWindowShouldClose(window)) {
 
         glfwPollEvents();
@@ -213,8 +196,8 @@ int main(int argc, const char* argv[]){
 
         glBindVertexArray(VAO);
         // glDrawArrays(GL_TRIANGLES, 0, 9);
-        // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-        glDrawElements(GL_TRIANGLES, sizeof(indices2) / sizeof(GLuint), GL_UNSIGNED_INT, 0);
+        // glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+        glDrawElements(GL_TRIANGLES, sizeof(indices4) / sizeof(GLuint), GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
 
         glfwSwapBuffers(window);
