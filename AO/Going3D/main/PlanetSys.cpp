fc221651393d653971dea261a7a44627ec4fbbe8
Anton Ostrovskiy <ao.omega2000@gmail.com>
2017-09-15 14:26:50 -0500

    Globe Rotation

--- AO/Going3D/main/PlanetSys.cpp
+++ AO/Going3D/main/PlanetSys.cpp
@@ -19,21 +19,25 @@ namespace Key {
 	GLboolean W, A, S, D, Q, E, O, P, K, L = false;
 }
 
+namespace Globe {
+	GLfloat size = 600.0f;
+	GLuint slices = 300;
+	GLuint stacks = 300;
+	GLboolean isRotated = false;
+	glm::vec3 rotation = glm::vec3(0.0, 0.0, 0.0);
+	glm::mat4 rotationMatrix(1);
+}
+
 namespace Player {
 	GLfloat height = 3.0f; 
 	GLfloat movementSpeed = 10.0f;
 	GLuint vRotationSpeed = 120;
 	GLuint hRotationSpeed = 90;
+	// glm::vec3 camera = glm::vec3(0.0, Globe::size + Player::height, 0.0f);
+	glm::vec3 camera = glm::vec3(0.0, 0.0f, -1200.0f);
 }
 
-namespace Globe {
-	GLfloat size = 600.0f;
-	GLuint slices = 300;
-	GLuint stacks = 300;
-}
-
-// glm::vec3 cameraPos = glm::vec3(0.0, 0.0, -300.0f);
-glm::vec3 cameraPos = glm::vec3(0.0, Globe::size + Player::height, 0.0f);
+// glm::vec3 Player::camera = glm::vec3(0.0, 0.0, -300.0f);
 glm::mat4 cameraRotation(1);
 GLboolean cursorPresent = true;	
 GLdouble cursorInitX, cursorInitY;
@@ -52,6 +56,43 @@ void cursorPosCallback(GLFWwindow* window, double xpos, double ypos){
 	cursorInitX = xpos;
 	cursorInitY = ypos;
 }
+
+/* void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+	if (key == GLFW_KEY_W && action == GLFW_PRESS) Key::W = true;
+	if (key == GLFW_KEY_A && action == GLFW_PRESS) Key::A = true;
+	if (key == GLFW_KEY_S && action == GLFW_PRESS) Key::S = true;
+	if (key == GLFW_KEY_D && action == GLFW_PRESS) Key::D = true;
+	if (key == GLFW_KEY_Q && action == GLFW_PRESS) Key::Q = true;
+	if (key == GLFW_KEY_E && action == GLFW_PRESS) Key::E = true;
+	if (key == GLFW_KEY_O && action == GLFW_PRESS) Key::O = true;
+	if (key == GLFW_KEY_P && action == GLFW_PRESS) Key::P = true;
+	if (key == GLFW_KEY_K && action == GLFW_PRESS) Key::K = true;
+	if (key == GLFW_KEY_L && action == GLFW_PRESS) Key::L = true;
+
+	if (key == GLFW_KEY_W && action == GLFW_RELEASE) Key::W = false;
+	if (key == GLFW_KEY_A && action == GLFW_RELEASE) Key::A = false;
+	if (key == GLFW_KEY_S && action == GLFW_RELEASE) Key::S = false;
+	if (key == GLFW_KEY_D && action == GLFW_RELEASE) Key::D = false;
+	if (key == GLFW_KEY_Q && action == GLFW_RELEASE) Key::Q = false;
+	if (key == GLFW_KEY_E && action == GLFW_RELEASE) Key::E = false;
+	if (key == GLFW_KEY_O && action == GLFW_RELEASE) Key::O = false;
+	if (key == GLFW_KEY_P && action == GLFW_RELEASE) Key::P = false;
+	if (key == GLFW_KEY_K && action == GLFW_RELEASE) Key::K = false;
+	if (key == GLFW_KEY_L && action == GLFW_RELEASE) Key::L = false;
+
+	if(Key::W) Player::camera.z += Player::movementSpeed; 
+	if(Key::A) Player::camera.x += Player::movementSpeed;
+	if(Key::S) Player::camera.z -= Player::movementSpeed;
+	if(Key::D) Player::camera.x -= Player::movementSpeed;
+	if(Key::Q) Player::camera.y -= Player::movementSpeed;
+	if(Key::E) Player::camera.y += Player::movementSpeed;
+	if(Key::O) Globe::slices++;
+	if(Key::P) Globe::slices--;
+	if(Key::K) Globe::stacks++;
+	if(Key::L) Globe::stacks--;
+	if(Key::O || Key::P || Key::K || Key::L) 
+	std::cout << "Size: " << Globe::size << " Slices: " << Globe::slices << " Stacks: " << Globe::stacks << std::endl;
+} */
 
 void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
 	if (key == GLFW_KEY_W && action == GLFW_PRESS) Key::W = true;
@@ -76,12 +117,14 @@ void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods
 	if (key == GLFW_KEY_K && action == GLFW_RELEASE) Key::K = false;
 	if (key == GLFW_KEY_L && action == GLFW_RELEASE) Key::L = false;
 
-	if(Key::W) cameraPos.z += Player::movementSpeed; 
-	if(Key::A) cameraPos.x += Player::movementSpeed;
-	if(Key::S) cameraPos.z -= Player::movementSpeed;
-	if(Key::D) cameraPos.x -= Player::movementSpeed;
-	if(Key::Q) cameraPos.y -= Player::movementSpeed;
-	if(Key::E) cameraPos.y += Player::movementSpeed;
+	if(Key::W) Globe::rotation.x = 1.0; 
+	if(Key::A) Globe::rotation.y = 1.0;
+	if(Key::S) Globe::rotation.x = -1.0;
+	if(Key::D) Globe::rotation.y = -1.0;
+	if(Key::W || Key::A || Key::S || Key::D)
+	Globe::isRotated = true;
+	if(Key::Q) Player::camera.y -= Player::movementSpeed;
+	if(Key::E) Player::camera.y += Player::movementSpeed;
 	if(Key::O) Globe::slices++;
 	if(Key::P) Globe::slices--;
 	if(Key::K) Globe::stacks++;
@@ -168,7 +211,7 @@ int main(int argc, char** argv){
 		{0.76f, 0.913f, 0.96f}
 	};
 
-	std::string sphereTex_filePath = parentDir + "\\..\\..\\data\\SandTex1.ktx";
+	std::string sphereTex_filePath = parentDir + "\\..\\..\\data\\MipTest.ktx";
 	GLuint sphereTex = createTexture(sphereTex_filePath.c_str());
 
 	ModelComposite Sphere;
@@ -178,12 +221,17 @@ int main(int argc, char** argv){
 		glfwPollEvents();
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		lookPos = glm::vec3(cameraPos.x + std::sin(hAngle), cameraPos.y + std::sin(vAngle), cameraPos.z + std::cos(hAngle + vAngle));
-		mvMatrix = glm::lookAt(cameraPos, lookPos, glm::vec3(0.0, 1.0, 0.0));
+		lookPos = glm::vec3(Player::camera.x + std::sin(hAngle), Player::camera.y + std::sin(vAngle), Player::camera.z + std::cos(hAngle + vAngle));
+		if(Globe::isRotated){
+			Globe::rotationMatrix = glm::rotate(Globe::rotationMatrix, Player::movementSpeed / 600, Globe::rotation);
+			Globe::rotation = glm::vec3(0.0, 0.0, 0.0);
+			Globe::isRotated = false;
+		}
+		mvMatrix = glm::lookAt(Player::camera, lookPos, glm::vec3(0.0, 1.0, 0.0)) * Globe::rotationMatrix;
 
 		sphereUtil.mvpMatrix(perspectiveMatrix * mvMatrix);
 		sphereUtil.nMatrix(glm::mat3(glm::transpose(glm::inverse(mvMatrix))));
-		sphereUtil.renderMode(2);
+		sphereUtil.renderMode(1);
 		sphereUtil.colorPalette(&coolPalette);
 		glBindTextureUnit(0, sphereTex);
 		glBindVertexArray(Sphere.VertexArray);
